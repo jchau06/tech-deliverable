8b895bcb0fa48f180df26ebed1869deed32fdcc6
Jonathan Chau <chaujt1@uci.edu>
2025-11-16 20:06:27 -0800

    Reworked App for two-column organization.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,21 +3,22 @@ import Header from "./components/Header.jsx";
 import QuoteFilter from "./components/QuoteFilter.jsx";
 import QuoteForm from "./components/QuoteForm.jsx";
 import QuoteList from "./components/QuoteList.jsx";
+import { Flex, Box } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 
 function App() {
   const [filterValue, setFilterValue] = useState("all");
   const [quotes, setQuotes] = useState([]);
-  
-  const fetchQuotes = async () => {
-	let url = "/api/quotes";
-	if (filterValue && filterValue !== "all") {
-	  url += `?max_age=${filterValue}`;
-	}
 
-	const res = await fetch(url);
-	const data = await res.json();
-	setQuotes(data);
+  const fetchQuotes = async () => {
+    let url = "/api/quotes";
+    if (filterValue && filterValue !== "all") {
+      url += `?max_age=${filterValue}`;
+    }
+
+    const res = await fetch(url);
+    const data = await res.json();
+    setQuotes(data);
   };
 
   useEffect(() => {
@@ -28,17 +29,21 @@ function App() {
     fetchQuotes();
   };
 
-
   return (
     <div className="App">
-      {/* TODO: include an icon for the quote book */}
       <Header />
-      <QuoteFilter
-        value={filterValue}
-        onChange={(e) => setFilterValue(e.target.value)}
-      />
-       <QuoteForm onSubmitSuccess={handleNewQuote} />
-      <QuoteList quotes={quotes} />
+      <Flex gap={6} align="flex-start">
+        <Box flex="1" maxWidth="30vw">
+          <QuoteFilter
+            value={filterValue}
+            onChange={(e) => setFilterValue(e.target.value)}
+          />
+          <QuoteForm onSubmitSuccess={handleNewQuote} />
+        </Box>
+        <Box flex="2" maxHeight="65vh" overflowY="auto">
+          <QuoteList quotes={quotes} />
+        </Box>
+      </Flex>
     </div>
   );
 }
