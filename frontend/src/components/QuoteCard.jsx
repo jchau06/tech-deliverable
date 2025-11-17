722c9a3061dc3bd0b57bc332bbe0a9418af21ad5
Jonathan Chau <chaujt1@uci.edu>
2025-11-16 20:49:42 -0800

    Finalized Styling for QuoteCard and QuoteList, updated time signatures for posts.

--- frontend/src/components/QuoteCard.jsx
+++ frontend/src/components/QuoteCard.jsx
@@ -1,11 +1,23 @@
 import { Box, Text } from "@chakra-ui/react";
 
 export default function QuoteCard({ quote }) {
-    return (
-        <Box p={4} borderWidth="1px" borderRadius="md" shadow="sm" bg="white">
+  return (
+    <Box
+      p={4}
+      borderWidth="1px"
+      borderRadius="md"
+      shadow="sm"
+      bg="#FFF7F5"
+      maxWidth="65vw"
+    >
       <Text fontWeight="bold">{quote.name}</Text>
       <Text>{quote.message}</Text>
-      <Text fontSize="sm" color="gray.500">{quote.time}</Text>
+      <Text fontSize="sm" color="gray.500">
+        {new Date(quote.time).toLocaleString(undefined, {
+          dateStyle: "medium",
+          timeStyle: "short",
+        })}
+      </Text>
     </Box>
-    )
+  );
 }
--- frontend/src/components/QuoteList.jsx
+++ frontend/src/components/QuoteList.jsx
@@ -2,12 +2,14 @@ import { VStack, Text } from "@chakra-ui/react";
 import QuoteCard from "./QuoteCard";
 
 export default function QuoteList({ quotes }) {
-    return (
-        <VStack spacing={4} align="stretch">
-      <Text fontSize="xl" fontWeight="bold">Previous Quotes</Text>
+  return (
+    <VStack spacing={4} align="stretch">
+      <Text fontSize="xl" fontWeight="bold" color="#EEDDE5" marginBottom={-1.5}>
+        Previous Quotes
+      </Text>
       {quotes.map((quote) => (
         <QuoteCard key={quote.time} quote={quote} />
       ))}
     </VStack>
-    )
+  );
 }
