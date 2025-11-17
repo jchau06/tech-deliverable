1eb763b4e20a312e680e74026094ed7849ed9c9c
Jonathan Chau <chaujt1@uci.edu>
2025-11-16 20:17:35 -0800

    Styling changes to Header and QuoteFilter

--- frontend/src/components/QuoteFilter.jsx
+++ frontend/src/components/QuoteFilter.jsx
@@ -3,15 +3,23 @@ import { Box, FormControl, FormLabel, Select } from "@chakra-ui/react";
 export default function QuoteFilter({ value, onChange }) {
   
   return (
-    <Box marginBottom={4}>
+    <Box 
+    display="flex"
+    alignItems="center"
+    paddingLeft={4}
+    paddingRight={4}
+    flexDirection="column">
       <FormControl>
-        <FormLabel fontWeight="bold">Filter Quotes</FormLabel>
+        <FormLabel fontWeight="bold" color="#EEDDE5" fontSize="xl">Filter Quotes</FormLabel>
         <Select
           value={value}
           onChange={onChange}
           maxWidth="240px"
           borderRadius="md"
           variant="filled"
+          bg="#FFF7F5"   
+          _focus={{ bg: "#FFF7F5" }}  
+          _hover={{ bg: "#FBEDEA" }}   
         >
           <option value="all">All Quotes</option>
           <option value="7">Last Week</option>
